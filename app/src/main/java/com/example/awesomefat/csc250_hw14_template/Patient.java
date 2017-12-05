09319b0bcab466e18b35bdc4a54229722ffa8383
Jappel97 <32598921+Jappel97@users.noreply.github.com>
2017-12-05 09:58:25 -0600

    Did the thing

--- app/src/main/java/com/example/awesomefat/csc250_hw14_template/Patient.java
+++ app/src/main/java/com/example/awesomefat/csc250_hw14_template/Patient.java
@@ -1,10 +1,14 @@
 package com.example.awesomefat.csc250_hw14_template;
 
+import android.support.annotation.NonNull;
+
+import java.util.Arrays;
+
 /**
  * Created by awesomefat on 11/30/17.
  */
 
-public class Patient extends Person
+public class Patient extends Person implements Comparable<Patient>
 {
     private double balance;
 
@@ -17,5 +21,17 @@ public class Patient extends Person
     public String toString()
     {
         return String.format("%s Balance: %f", super.toString(), this.balance);
+    }
+
+    public void patientSort(Patient[] people)
+    {
+        Arrays.sort(people);
+    }
+
+    @Override
+    public int compareTo(@NonNull Patient thisGuy)
+    {
+        int compBalance = (int)thisGuy.balance;
+        return (int)this.balance - compBalance;
     }
 }
